58aa9a01725013758d02f88df145d6ca7df4a0f3
kazeneza-zephilin <kzephilin2@gmail.com>
2024-10-14 20:09:46 +0200

    adding asncy file

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import asyncio
+
+"""running multiple coroutine at the same time
+"""
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """call wait_random n times with specified max_delay"""
+    num_collection = []
+    for e in range(n):
+        tasks = [wait_random(max_delay) for t in range(n)]
+        tasks = asyncio.as_completed(tasks)
+        task_collection = [await task for task in tasks]
+        return task_collection
