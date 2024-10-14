73bd46e54cc597bc557ad1af72bf1954e4cb9b13
kazeneza-zephilin <kzephilin2@gmail.com>
2024-10-14 20:27:49 +0200

    adding asncy file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,15 +3,12 @@
 """
 from typing import List
 import asyncio
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """call wait_random n times with specified max_delay"""
-    num_collection = []
-    for e in range(n):
-        tasks = [wait_random(max_delay) for t in range(n)]
-        tasks = asyncio.as_completed(tasks)
-        task_collection = [await task for task in tasks]
-        return task_collection
+    tasks = [wait_random(max_delay) for t in range(n)]
+    tasks = asyncio.as_completed(tasks)
+    task_collection = [await task for task in tasks]
+    return task_collection
