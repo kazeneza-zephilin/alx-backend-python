edd9317433b734ff0dd0bbf70543151af84d506a
kazeneza-zephilin <kzephilin2@gmail.com>
2024-10-15 20:13:04 +0200

    adding async comprehension file

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""async comprehension"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+async def async_generator() -> AsyncGenerator[float]:
+    """wait 1 sec asynchronous then yield random number"""
+    for i in range(10):
+        res = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield res
